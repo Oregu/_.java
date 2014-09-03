4565f1cfb94afe4e1d309b5583500d80ee4eca40
Dmitriy Khvatov <dkhvatov@playteam.ru>
2014-09-03 18:31:57 +0400

    TernaryFunction javadoc added

--- _.java
+++ _.java
@@ -34,6 +34,20 @@ static <CT extends List<T>, T, R> List<R> map(Function<T, R> f, CT coll) {
   return coll.stream().map(f).collect(toList());
 }
 
+/**
+ * Represents a function that accepts three arguments and produces a result.
+ * This is the three-arity specialization of {@link Function}.
+ * <p/>
+ * <p>This is a functional interface whose
+ * functional method is {@link #apply(Object, Object, Object)}.
+ *
+ * @param <T> the type of the first argument to the function
+ * @param <U> the type of the second argument to the function
+ * @param <V> the type of the third argument to the function
+ * @param <R> the type of the result of the function
+ * @see BiFunction
+ * @see Function
+ */
 @FunctionalInterface
 public interface TernaryFunction<T, U, V, R> {
 
